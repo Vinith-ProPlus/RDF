a5a3a76b5bbc7accf3fc9ad2aeb4f64e35fa23ca
Vinith Kumar <vinithkumarpropluslogics@gmail.com>
2024-08-22 09:47:21 +0530

    Update transaction.php

--- app/routes/web/transaction.php
+++ app/routes/web/transaction.php
@@ -1,49 +1,49 @@
 <?php
 
-use App\Http\Controllers\web\Transaction\EnquiryController;
+//use App\Http\Controllers\web\Transaction\EnquiryController;
 use App\Http\Controllers\web\Transaction\OrderController;
 use App\Http\Controllers\web\Transaction\QuoteEnquiryController;
 
-Route::group(['prefix'=>'enquiry'],function (){
-    Route::controller(EnquiryController::class)->group(function () {
-        Route::get('/', 'view');
-        Route::get('/trash', 'TrashView');
-        Route::get('/create', 'Create');
-        Route::get('/edit/{ID}', 'Edit');
-        Route::get('/view/{ID}', 'EnqView');
+//Route::group(['prefix'=>'enquiry'],function (){
+//    Route::controller(EnquiryController::class)->group(function () {
+//        Route::get('/', 'view');
+//        Route::get('/trash', 'TrashView');
+//        Route::get('/create', 'Create');
+//        Route::get('/edit/{ID}', 'Edit');
+//        Route::get('/view/{ID}', 'EnqView');
+//
+//        Route::POST('/convert/{ID}', 'Convert');
+//        Route::POST('/delete/{ID}', 'Delete');
+//        Route::POST('/restore/{ID}', 'Restore');
+//
+//        Route::post('/data', 'TableView');
+//        Route::post('/trash-data', 'TrashTableView');
+//    });
+//});
 
-        Route::POST('/convert/{ID}', 'Convert');
-        Route::POST('/delete/{ID}', 'Delete');
-        Route::POST('/restore/{ID}', 'Restore');
-
-        Route::post('/data', 'TableView');
-        Route::post('/trash-data', 'TrashTableView');
-    });
-});
-
-Route::group(['prefix'=>'quote-enquiry'],function (){
-    Route::controller(QuoteEnquiryController::class)->group(function () {
-        Route::get('/', 'view');
-        Route::get('/trash', 'TrashView');
-        Route::get('/create', 'Create');
-        Route::get('/edit/{ID}', 'Edit');
-        Route::get('/view/{ID}', 'QuoteView');
-        Route::get('/view/vendor-quote/{ID}', 'VendorQuoteView');
-
-        Route::post('/data', 'TableView');
-        Route::post('/request-quote/{ID}', 'RequestQuote');
-        Route::POST('/quote-convert/{ID}', 'QuoteConvert');
-        Route::POST('/delete/{ID}', 'Delete');
-        Route::POST('/restore/{ID}', 'Restore');
-        Route::post('/trash-data', 'TrashTableView');
-
-        Route::post('/get/Quotation', 'GetQuotation');
-        Route::post('/get/vendor-quote-details', 'GetVendorQuoteDetails');
-        Route::post('/get/vendor-ratings', 'GetVendorRatings');
-
-        Route::post('/get/customers', 'GetCustomers');
-    });
-});
+//Route::group(['prefix'=>'quote-enquiry'],function (){
+//    Route::controller(QuoteEnquiryController::class)->group(function () {
+//        Route::get('/', 'view');
+//        Route::get('/trash', 'TrashView');
+//        Route::get('/create', 'Create');
+//        Route::get('/edit/{ID}', 'Edit');
+//        Route::get('/view/{ID}', 'QuoteView');
+//        Route::get('/view/vendor-quote/{ID}', 'VendorQuoteView');
+//
+//        Route::post('/data', 'TableView');
+//        Route::post('/request-quote/{ID}', 'RequestQuote');
+//        Route::POST('/quote-convert/{ID}', 'QuoteConvert');
+//        Route::POST('/delete/{ID}', 'Delete');
+//        Route::POST('/restore/{ID}', 'Restore');
+//        Route::post('/trash-data', 'TrashTableView');
+//
+//        Route::post('/get/Quotation', 'GetQuotation');
+//        Route::post('/get/vendor-quote-details', 'GetVendorQuoteDetails');
+//        Route::post('/get/vendor-ratings', 'GetVendorRatings');
+//
+//        Route::post('/get/customers', 'GetCustomers');
+//    });
+//});
 
 Route::group(['prefix' => 'order'], function () {
     Route::get('/', [OrderController::class, 'view']);
